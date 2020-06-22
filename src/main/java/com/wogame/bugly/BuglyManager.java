4e123396c4f0739ed22d534df15b70a389af8bb3
Jadekylin <xingyi881018@gmail.com>
2020-06-22 17:37:54 +0800

    bugly

--- src/main/java/com/wogame/bugly/BuglyManager.java
+++ src/main/java/com/wogame/bugly/BuglyManager.java
@@ -1,10 +1,10 @@
-package com.wogameo.bugly;
+package com.wogame.bugly;
 
 import android.content.Context;
 
 import com.tencent.bugly.crashreport.CrashReport;
-import com.wogame.common.Common;
 import com.wogame.util.AppInfoUtil;
+
 
 public class BuglyManager {
     private static BuglyManager instance;
@@ -16,9 +16,14 @@ public class BuglyManager {
         return instance;
     }
 
-    public void initWithApplication(Context _context,String buglyId){
-        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(_context.getApplicationContext());
-        strategy.setAppChannel(AppInfoUtil.m_channelId);  //设置渠道
-        CrashReport.initCrashReport(_context.getApplicationContext(), buglyId, false, strategy);
+    public void initApplication(Context context,String buglyId){
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context.getApplicationContext());
+        CrashReport.initCrashReport(context.getApplicationContext(), buglyId, false, strategy);
+
+        strategy.setAppChannel(AppInfoUtil.mChannelId); //设置渠道
+        strategy.setAppVersion(AppInfoUtil.getVersionName()); //App的版本
+        strategy.setAppPackageName(AppInfoUtil.mPackageName); //App的包名
+        //Bugly会在启动10s后联网同步数据。若您有特别需求，可以修改这个时间
+        //strategy.setAppReportDelay(20000); //改为20s
     }
 }
